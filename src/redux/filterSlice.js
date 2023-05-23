d45726ef9bf25f71ae5625980a67805e57a18227
SerhiiRud <carmagsales@gmail.com>
2023-05-23 14:17:43 +0300

    finished

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const filterSlice = createSlice({
   name: 'filter',
-  inititalState: '',
+  initialState: '',
   reducers: {
     setFilter: (state, action) => {
       return action.payload;
